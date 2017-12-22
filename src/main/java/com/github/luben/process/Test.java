e8958a0b4ee977b06ffb31313ea0a4aa7982f0f4
Eric PASCAL <epascal@localhost.localdomain>
2017-12-22 18:20:47 +0100

    Test class

---
+++ src/main/java/com/github/luben/process/Test.java
@@ -0,0 +1,13 @@
+import com.github.luben.process.Process;
+
+public class Test
+{ 
+	public static void main(String [] args) {
+        String[] groups = Process.getgrouplist(args[0]);
+        for (String group : groups) {
+            System.out.println(group);
+        }
+        boolean logged = Process.login(args[0], args[1]);
+        System.out.println(logged);
+    }
+}
